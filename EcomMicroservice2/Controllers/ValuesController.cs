8f58d32de871af6de7c23f5b4b9fd0e862732153
sabitra123 <44191742+sabitra123@users.noreply.github.com>
2019-12-13 20:18:38 +0530

    reverted back home controller changes
    reverted back home controller changes

--- EcomMicroservice2/Controllers/ValuesController.cs
+++ EcomMicroservice2/Controllers/ValuesController.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using MySql.Data.MySqlClient;
-using Microsoft.Extensions.Configuration;
-using System.Text;
 
 namespace EcomMicroservice2.Controllers
 {
@@ -13,56 +10,19 @@ namespace EcomMicroservice2.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-
-        public IConfiguration Configuration { get; }
-        public ValuesController(IConfiguration _configuration)
-        {
-            Configuration = _configuration;
-        }
-
         // GET api/values
-        [HttpGet("{searchString}/{id}")]
-        public ActionResult<IEnumerable<string>> Get(string searchString, Int32 id)
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(Int32 id)
+        public ActionResult<string> Get(int id)
         {
-        try{
-                 StringBuilder sb = new StringBuilder();
-                using (MySqlConnection conn = GetConnection())  
-                {  
-                    
-                    conn.Open();  
-                    MySqlCommand cmd = new MySqlCommand("select DISTINCT SEGMENT_NAME,FAMILY_NAME,CLASS_NAME from XXIBM_PRODUCT_CATALOGUE ", conn);                
-                    MySqlDataReader dataReader = cmd.ExecuteReader();
-
-                    while (dataReader.Read())  
-                    {  
-                       
-			            sb.Append(Convert.ToString(dataReader["SEGMENT_NAME"]));
-                       
-                        sb.Append(Convert.ToString(dataReader["FAMILY_NAME"]));
-                      
-                        sb.Append(Convert.ToString(dataReader["CLASS_NAME"]));
-                       
-                    } 
-                }
-            return sb.ToString();
-            }
-            catch(Exception ex)
-            {
-               return ex.InnerException + ex.Message + ex.StackTrace + Configuration["ConnectionStrings:Default"];
-            }
+            return "value";
         }
-
-        private MySqlConnection GetConnection()    
-        {    
-            return new MySqlConnection(Configuration["ConnectionStrings:Default"]);    
-        } 
 
         // POST api/values
         [HttpPost]
