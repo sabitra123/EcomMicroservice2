ba67e97fd965e1d228518566eb72b7c587706dfa
sabitra123 <44191742+sabitra123@users.noreply.github.com>
2019-12-18 12:37:33 +0530

    getting XXIBM_PRODUCT_SKU column details
    XXIBM_PRODUCT_SKU

--- EcomMicroservice2/Models/ModelQueryString.cs
+++ EcomMicroservice2/Models/ModelQueryString.cs
@@ -8,7 +8,7 @@ namespace EcomMicroservice2.Models
         public static string getDistinctFamilyProduct = "select DISTINCT FAMILY, FAMILY_NAME from XXIBM_PRODUCT_CATALOGUE WHERE SEGMENT=@SEGMENT ORDER BY FAMILY";
         public static string getDistinctClassProduct = "select DISTINCT CLASS, CLASS_NAME from XXIBM_PRODUCT_CATALOGUE WHERE FAMILY=@FAMILY  ORDER BY CLASS";
         public static string getDistinctCommodityProduct = "select DISTINCT COMMODITY, COMMODITY_NAME from XXIBM_PRODUCT_CATALOGUE WHERE CLASS=@CLASS  ORDER BY COMMODITY";
-        public static string getDistinctBrandProduct = "select DISTINCT BRAND from XXIBM_PRODUCT_STYLE  ORDER BY BRAND";
+        public static string getDistinctBrandProduct = "SHOW COLUMNS AS BRAND FROM XXIBM_PRODUCT_SKU"; //"select DISTINCT BRAND from XXIBM_PRODUCT_STYLE  ORDER BY BRAND";
 
         public static string getAllProductWithDetails = " Select FAMILY_NAME, CLASS_NAME, COMMODITY, COMMODITY_NAME,SKU.ITEM_NUMBER, SKU.DESCRIPTION,SKU.LONG_DESCRIPTION,BRAND,SKUAtt_Value1 AS SIZE, SKUAtt_Value2 AS COLOUR, LIST_PRICE,DISCOUNT,INSTOCK,PRICE_EFFECTIVE_DATE " +
                                                         " FROM XXIBM_PRODUCT_CATALOGUE AS CATALOGUE " +
