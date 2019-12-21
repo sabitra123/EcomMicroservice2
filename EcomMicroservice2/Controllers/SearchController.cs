ab6db0bb64ab4d163294aa1e02c8d804e298797d
sabitra123 <44191742+sabitra123@users.noreply.github.com>
2019-12-21 23:12:51 +0530

    Search functionality has been added .
    Search functionality has been added .

---
+++ EcomMicroservice2/Controllers/SearchController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using EcomMicroservice2.Models;
+
+namespace EcomMicroservice2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AddHeader("Access-Control-Allow-Origin", "*")]
+    public class SearchController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+        public SearchController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+        // JsonResult
+        //Family,Class,Commodity,Color,Brand
+        [HttpGet("{SearchValue}")]
+        public JsonResult Get(string SearchValue)
+        {
+            DatabaseCURD dbCurd = new DatabaseCURD();
+            List<ProductDetailsClass> lst = dbCurd.GetSearchProductDetails(Configuration["ConnectionStrings:Default"],SearchValue);
+            return new JsonResult(lst);  
+        }
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
